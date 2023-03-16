9ed5ee666d62d62b68e362a87b75b59cd896f2b9
noel2017mdala <noelmdala2017@gmail.com>
2023-03-16 22:48:25 +0200

    added header and banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import { Genre, Movie, Element } from "../../../types/types";
+import Image from "next/image";
+import { baseUrl } from "../../../constants/constants";
+import { FaPlay } from "react-icons/fa";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+type Props = {
+  netflixOriginals: Movie[];
+};
+
+const index = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
+
+  return (
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className="absolute top-0 left-0 h-[95vh] -z-10 w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          alt={""}
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+
+      <div className="m-8">
+        <h1 className="text-2xl md:text-4xl  lg:text-6xl font-bold">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <p className="mx-w-xs mt-4 text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+          {movie?.overview}
+        </p>
+      </div>
+
+      <div className="flex  space-x-8 m-8">
+        <button className="bannerButton bg-white text-black">
+          <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
+        </button>
+        <button className="bannerButton bg-[gray]/70">
+          More Info
+          <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default index;
